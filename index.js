9e768324d0cd13a13490c77a6b998fcf53c5355c
unknown <antoniotx.dev@gmail.com>
2019-05-08 20:40:22 -0300

    configura database

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 const app = express()
+
+//Bodyparser Middleware
+app.use(bodyParser.json())
+
+const db = 'mongodb://<dbuser>:<dbpassword>@ds153566.mlab.com:53566/shortcode-populate'
+
+mongoose.connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Conectado'))
+  .catch(erro => console.log(erro))
 
 const port = 5000
 
