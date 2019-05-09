35bbbe490b052c37327fe09f21dd27cd7f3aff1e
unknown <antoniotx.dev@gmail.com>
2019-05-08 21:25:48 -0300

    Resolve bug do update no mongoose

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json())
 
 const db = 'mongodb://antonio:a12345@ds153566.mlab.com:53566/shortcode-populate'
 
-mongoose.connect(db, { useNewUrlParser: true })
+mongoose.connect(db,{ useNewUrlParser: true, useFindAndModify: false })
   .then(() => console.log('MongoDB Conectado'))
   .catch(erro => console.log(erro))
 
